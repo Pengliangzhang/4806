9614e6d92571a27881ac522ed0ce77751d2d0c1d
pengliangzhang <pengliangzhang@LABS.SCE.CARLETON.CA>
2020-02-05 16:00:42 -0500

    test

--- src/main/java/Lab/WebController.java
+++ src/main/java/Lab/WebController.java
@@ -63,5 +63,11 @@ public class WebController {
         return "DeletedPage";
     }
 
+    @GetMapping("/error")
+    public String error(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "homePage"; //view
+    }
+
 
 }
