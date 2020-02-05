5e16ce7c0e0ce9a5170a1e5caba5e127c88f8704
pengliangzhang <pengliangzhang@LABS.SCE.CARLETON.CA>
2020-02-05 15:21:23 -0500

    test

--- src/main/java/Lab/RestLabApplication.java
+++ src/main/java/Lab/RestLabApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestLabApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(RestLabApplication.class, args);
 	}
